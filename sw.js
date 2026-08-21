7764d85a981566349564de920e4f498bc8bd971a
sayeed2758 <sayeedbackupmail@gmail.com>
2026-08-21 21:20:54 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="sayeed-academy-v6-premium-20260821";
+const CACHE="sayeed-academy-v6-premium-20260821-7";
 const CORE=["./","./index.html","./assets/css/style.css","./assets/js/app.js","./data/catalog.json","./manifest.webmanifest","./assets/icons/logo.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
